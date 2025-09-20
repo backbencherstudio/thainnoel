3bdbb9cdcec20906252fa49376aa19a6c70046af
shurov <shurov.0.00.0@gmail.com>
2025-09-20 10:55:54 +0600

    Solution added

--- app/servicedetailss/page.tsx
+++ app/servicedetailss/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ServiceBoost from './components/detailboost'
 import { HowItWorksSection } from './components/HowItWorksSection'
 import WhatsIncluded from './components/whatsIncluded'
+import { SolutionSection } from './components/SolutionSection'
 
 export default function ServiceDetailsPage() {
   return (
@@ -13,6 +14,7 @@ export default function ServiceDetailsPage() {
         <ServiceBoost />
         <HowItWorksSection />
         <WhatsIncluded />
+        <SolutionSection />
     </div>
   )
 }
